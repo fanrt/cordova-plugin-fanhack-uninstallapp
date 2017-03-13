31c9d049ca9136e14ab2ffb7bcf33dd8e3c3314e
fanrt <876992687@qq.com>
2017-03-13 14:35:59 +0800

    把对比版本号和卸载分开为两个方法，分别为checkAppVersion和uninstall

--- src/android/UninstallOtherAppPlugin.java
+++ src/android/UninstallOtherAppPlugin.java
@@ -1,65 +1,72 @@
 
-package com.fanrt.plugin;  
-  
-import org.apache.cordova.CallbackContext;  
-import org.apache.cordova.CordovaPlugin;  
-import org.apache.cordova.PluginResult;  
-import org.json.JSONArray;  
-import org.json.JSONException;  
-  
-import android.app.Activity;  
-import android.content.ComponentName;  
-import android.content.Intent;  
-import android.os.Bundle;  
-import android.util.Log;  
+package com.fanrt.plugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.content.Intent;
 import android.net.Uri;
-  
-public class UninstallOtherAppPlugin extends CordovaPlugin {  
-	public static final String ACTION = "uninstall";  
-	  
-	@Override  
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {  
-		if (action.equals(ACTION)) {
-			String appPackage = args.getString(0);
-			String appVersion = args.getString(1);
-			checkApp(appPackage, appVersion);
-			return true;
-		}  
-  
-		return true;  
-	}
 
-	// Thanks to http://floresosvaldo.com/android-cordova-plugin-checking-if-an-app-exists
-	public boolean appInstalled(String appPackage, String appVersion) {
-		Context ctx = this.cordova.getActivity().getApplicationContext();
-		final PackageManager pm = ctx.getPackageManager();
-		boolean app_installed = false;
-		try {
-			pm.getPackageInfo(appPackage, PackageManager.GET_ACTIVITIES);
-			ApplicationInfo app = packageManager.getApplicationInfo(appPackage, 0);
-			if (appVersion.equals(app.versionCode)) {
-				app_installed = true;
-			} else {
-				Intent intent = new Intent();
-				intent.setAction(Intent.ACTION_DELETE);
-				intent.setData(Uri.parse("package:" + appPackage));
-				cordova.getActivity().startActivity(intent);
-				app_installed = false;
-			}
-		}
-		catch(PackageManager.NameNotFoundException e) {
-			app_installed = false;
-		}
-		return app_installed;
-	}
+public class UninstallOtherAppPlugin extends CordovaPlugin {
+    public static final String UNINSTALL_ACTION = "uninstall";
+    public static final String CHECKAPPVERSION_ACTION = "checkAppVersion";
 
-	private void checkApp(String appPackage, String appVersion, CallbackContext callbackContext) {
-		if(appUninstall(appPackage, appVersion)) {
-			callbackContext.success("1");
-		}
-		else {
-			callbackContext.error("0");
-		}
-	}
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals(UNINSTALL_ACTION)) {
+            String appPackage = args.getString(0);
+            appUninstall(appPackage, callbackContext);
+            return true;
+        } else if (action.equals(CHECKAPPVERSION_ACTION)) {
+            String appPackage = args.getString(0);
+            String appVersion = args.getString(1);
+            checkAppVersion(appPackage, appVersion, callbackContext);
+        }
+
+        return true;
+    }
+
+    // Thanks to http://floresosvaldo.com/android-cordova-plugin-checking-if-an-app-exists
+    private void appUninstall(String appPackage, CallbackContext callbackContext) {
+        Context ctx = this.cordova.getActivity().getApplicationContext();
+        final PackageManager pm = ctx.getPackageManager();
+        try {
+            pm.getPackageInfo(appPackage, PackageManager.GET_ACTIVITIES);
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_DELETE);
+            intent.setData(Uri.parse("package:" + appPackage));
+            cordova.getActivity().startActivity(intent);
+            callbackContext.success("uninstallSuccess");
+        }
+        catch(PackageManager.NameNotFoundException e) {
+            callbackContext.error("appNotFound");
+        }
+    }
+
+    private void checkAppVersion(String appPackage, String appVersion, CallbackContext callbackContext) {
+        Context ctx = this.cordova.getActivity().getApplicationContext();
+        final PackageManager pm = ctx.getPackageManager();
+        try {
+            pm.getPackageInfo(appPackage, PackageManager.GET_ACTIVITIES);
+            if (!appVersion.equals(pm.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionName)) {
+                callbackContext.success("correctApp");
+            } else {
+                callbackContext.error("versionError");
+            }
+        }
+        catch(PackageManager.NameNotFoundException e) {
+            callbackContext.error("appNotFound");
+        }
+    }
 }  
--- www/uninstallOtherApp.js
+++ www/uninstallOtherApp.js
@@ -4,9 +4,13 @@ var cordova = require('cordova');
   
 var UninstallOtherApp = function() {};  
   
-UninstallOtherApp.prototype.uninstall = function(uri, version,success, error) {
-    cordova.exec(success, error, 'UninstallOtherAppPlugin', 'uninstall', [uri, version]);
-};  
-  
+UninstallOtherApp.prototype.uninstall = function(uri, success, error) {
+    cordova.exec(success, error, 'UninstallOtherAppPlugin', 'uninstall', [uri]);
+};
+
+UninstallOtherApp.prototype.checkAppVersion = function(uri, version, success, error) {
+    cordova.exec(success, error, 'UninstallOtherAppPlugin', 'checkAppVersion', [uri, version]);
+};
+
 var uninstallOtherApp = new UninstallOtherApp();  
 module.exports = uninstallOtherApp; 
